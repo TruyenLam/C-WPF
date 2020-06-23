64dc7d586bdc92ff5ba10bc69e182724e9bb60b1
truyen.lv <lamvantruyen@gmail.com>
2020-06-23 23:16:07 +0700

    bai 14

---
+++ ObsExamps/ObsExamps/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ObsExamps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<String> obs = new ObservableCollection<String>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Add(object sender, RoutedEventArgs e)
+        {
+            obs.Add($"Mục mới thêm {obs.Count + 1}");
+            listview.ItemsSource = obs;
+        }
+
+        private void Remove(object sender, RoutedEventArgs e)
+        {
+            if (obs.Count > 0)
+                obs.RemoveAt(obs.Count - 1);
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            obs.Clear();
+        }
+    }
+}
--- QuanLyKho/QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
+++ QuanLyKho/QuanLyKho/QuanLyKho/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using QuanLyKho.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,9 @@ namespace QuanLyKho.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
+        private ObservableCollection<TonKho> _TonKhoList;
+        public ObservableCollection<TonKho> TonKhoList { get => _TonKhoList; set { _TonKhoList = value; OnPropertyChanged(); } }
+
         public bool Isloaded = false;
         #region commands
         public ICommand LoadedWindowCommand { get; set; }
@@ -24,12 +29,30 @@ namespace QuanLyKho.ViewModel
         // mọi thứ xử lý sẽ nằm trong này
         public MainViewModel()
         {
-            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
+            LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) => {
                 Isloaded = true;
+                if (p == null)
+                    return;
+                p.Hide();
                 LoginWindow loginWindow = new LoginWindow();
                 loginWindow.ShowDialog();
+
+                if (loginWindow.DataContext == null)
+                    return;
+                var loginVM = loginWindow.DataContext as LoginViewModel;
+
+                if (loginVM.IsLogin)
+                {
+                    p.Show();
+                    LoadTonKhoData();
+                }
+                else
+                {
+                    p.Close();
+                }
             }
-               );
+              );
+
             UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) => { UnitWindow wd = new UnitWindow(); wd.ShowDialog(); });
             SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) => { SuplierWindow wd = new SuplierWindow(); wd.ShowDialog(); });
             CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => { CustomerWindow wd = new CustomerWindow(); wd.ShowDialog(); });
@@ -38,6 +61,42 @@ namespace QuanLyKho.ViewModel
             InputCommand = new RelayCommand<object>((p) => { return true; }, (p) => { InputWindow wd = new InputWindow(); wd.ShowDialog(); });
             OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) => { OutputWindow wd = new OutputWindow(); wd.ShowDialog(); });
 
+            void LoadTonKhoData()
+            {
+                TonKhoList = new ObservableCollection<TonKho>();
+
+                var objectList = DataProvider.Ins.DB.Objects;
+
+                int i = 1;
+                foreach (var item in objectList)
+                {
+                    var inputList = DataProvider.Ins.DB.InputInfoes.Where(p => p.IdObject == item.Id);
+                    var outputList = DataProvider.Ins.DB.OutputInfoes.Where(p => p.IdObject == item.Id);
+
+                    int sumInput = 0;
+                    int sumOutput = 0;
+
+                    if (inputList != null)
+                    {
+                        sumInput = (int)inputList.Sum(p => p.Count);
+                    }
+                    if (outputList != null)
+                    {
+                        sumOutput = (int)outputList.Sum(p => p.Count);
+                    }
+
+                    TonKho tonkho = new TonKho();
+                    tonkho.STT = i;
+                    tonkho.Count = sumInput - sumOutput;
+                    tonkho.Object = item;
+
+                    TonKhoList.Add(tonkho);
+
+                    i++;
+                }
+
+            }
         }
     }
+    
 }
